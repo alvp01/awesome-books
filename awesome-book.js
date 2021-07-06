c4c0e8d2356f0b37cda03197d68c0534e5b2537c
errea <erreakezy@gmail.com>
2021-07-06 15:09:39 +0100

    Add function for local storage

--- awesome-book.js
+++ awesome-book.js
@@ -1,39 +1,50 @@
 const form  = document.getElementById('book-data');
 const bookTitle = document.querySelector('#book-title');
 const bookAuthor = document.querySelector('#book-author');
-const bookListed = document.querySelector('.book-list')
+const bookListed = document.querySelector('.book-list');
+const myBookStorage = window.localStorage;
+
 
 let Books = [];
+Books = Object.entries(myBookStorage);
 
-function addToList(title, author){
-  let li = document.createElement('li');
-  let div = document.createElement('div');
-  let btn = document.createElement('button');
+// function addToList(title, author){
+  
+// }
 
-  btn.innerHTML = "REMOVE BOOK";
-  btn.addEventListener('click', () => {
+function addToPage(title, author){
+
+    let titleBook = document.createElement('p')
+        titleBook.textContent = title;
+
+    let authorBook = document.createElement('p')
+        authorBook.textContent = author;
+        bookListed.appendChild(addToList(titleBook, authorBook));
+        localStorage.setItem(title, author);
+
+    let li = document.createElement('li');
+    let div = document.createElement('div');
+    let btn = document.createElement('button');
+
+    btn.innerHTML = "REMOVE BOOK";
+    btn.addEventListener('click', () => {
     removeBooks(btn.parentNode);
   });
 
-  div.appendChild(title);
-  div.appendChild(author);
-  div.appendChild(btn);
+    div.appendChild(title);
+    div.appendChild(author);
+    div.appendChild(btn);
 
-  li.appendChild(div);
-  return li;
-}
+    li.appendChild(div);
+    return li;
 
-function addToPage(title, author){
-  let titleBook = document.createElement('p')
-  titleBook.textContent = title;
-  let authorBook = document.createElement('p')
-  authorBook.textContent = author;
-  bookListed.appendChild(addToList(titleBook, authorBook));
 }
 
 function removeBooks(btn){
   li = btn.parentNode
   li.remove();
+  Books=Object.entries(myBookStorage);
+  Storage.removeItem(titleBook, authorBook);
 }
 
 form.addEventListener('submit', (event) => {
