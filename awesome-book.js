20897bbdf09df3469d087206b3fd9d8bfddefae3
Abel Lavieri <abel.lavieri@gmail.com>
2021-07-07 10:54:09 -0400

    Fix linter errors

--- awesome-book.js
+++ awesome-book.js
@@ -3,21 +3,21 @@ const bookAuthor = document.querySelector('#book-author');
 const bookListed = document.querySelector('.book-list');
 
 class Library {
-  constructor () {
-    this.books = new Array();
+  constructor() {
+    this.books = [];
   }
 
   setBooks() {
     this.books = JSON.parse(localStorage.getItem('books') || '[]');
   }
 
-  saveBooks() { 
+  saveBooks() {
     localStorage.setItem('books', JSON.stringify(this.books));
   }
 
-    displayBooks() {  
-      bookListed.innerHTML = '';
-      this.books.map((book) => {
+  displayBooks() {
+    bookListed.innerHTML = '';
+    this.books.map((book) => {
       const li = document.createElement('li');
       const div = document.createElement('div');
       const title = document.createElement('p');
@@ -36,33 +36,24 @@ class Library {
       bookListed.appendChild(li);
       return book;
     });
-    }
-
-    removeBooks(book) {
-      this.books = this.books.filter((b) => b.id !== book.id);
-      this.saveBooks();
-      this.displayBooks();
-    }
-
-    addBooks(author, title) { 
-      let book = new Book(author, title, this.books.length+1);
-      this.books.push(book);
-      this.displayBooks();
-      this.saveBooks();
-    }
   }
 
-class Book {
-  constructor(author, title,id) { 
-    this.author = author;
-    this.title = title;
-    this.id = id;
+  removeBooks(book) {
+    this.books = this.books.filter((b) => b.id !== book.id);
+    this.saveBooks();
+    this.displayBooks();
+  }
+
+  addBooks(author, title) {
+    const book = { author, title, id: this.books.length + 1 };
+    this.books.push(book);
+    this.displayBooks();
+    this.saveBooks();
   }
 }
 
-let library = new Library();
+const library = new Library();
 const form = document.getElementById('book-data');
-
 
 form.addEventListener('submit', () => {
   library.addBooks(bookAuthor.value, bookTitle.value);
