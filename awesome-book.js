b17e6b88ebefc026159c2c72f6b259505b6d63ff
errea <erreakezy@gmail.com>
2021-07-08 22:40:19 +0100

    Modify

--- awesome-book.js
+++ awesome-book.js
@@ -93,6 +93,15 @@ form.addEventListener('submit', () => {
   form.submit();
 });
 
+function responsiveMenu() {
+  var x = document.getElementById("navigation");
+  if (x.className === "navigation") {
+    x.className += " responsive";
+  } else {
+    x.className = "navigation";
+  }
+}
+
 window.onload = () => {
   library.setBooks();
   library.displayBooks();
